1e7bc59e3a632618abf972c1cee1376bdba64fce
Faheem akhter <68496070+ifaheemsid@users.noreply.github.com>
2023-10-02 13:54:01 +0530

    Add files via upload
    first commit

---
+++ flutter_login_page/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\srevi\Downloads\flutter_windows_3.13.6-stable\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\srevi\OneDrive\Desktop\projects\flutter_login_page"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
---
+++ flutter_login_page/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\srevi\\Downloads\\flutter_windows_3.13.6-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\srevi\\OneDrive\\Desktop\\projects\\flutter_login_page" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\srevi\\Downloads\\flutter_windows_3.13.6-stable\\flutter"
+  "PROJECT_DIR=C:\\Users\\srevi\\OneDrive\\Desktop\\projects\\flutter_login_page"
+  "FLUTTER_ROOT=C:\\Users\\srevi\\Downloads\\flutter_windows_3.13.6-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\srevi\\OneDrive\\Desktop\\projects\\flutter_login_page\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\srevi\\OneDrive\\Desktop\\projects\\flutter_login_page"
+  "FLUTTER_TARGET=C:\\Users\\srevi\\OneDrive\\Desktop\\projects\\flutter_login_page\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9hNzk0Y2YyNjgxYzZjOWZlN2IyNjBlMGU4NGRlOTYyOThkYzljMThiLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\srevi\\OneDrive\\Desktop\\projects\\flutter_login_page\\.dart_tool\\package_config.json"
+)
